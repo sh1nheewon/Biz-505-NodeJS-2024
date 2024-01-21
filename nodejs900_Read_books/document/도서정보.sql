d6964b8a17b2c7d141217919e39fac1b6368e511
sh1nheewon <tlsgmldnjs00@naver.com>
2024-01-21 17:57:24 +0900

    project

--- nodejs900_Read_books/document/도서정보.sql
+++ nodejs900_Read_books/document/도서정보.sql
@@ -24,9 +24,9 @@ CREATE TABLE tbl_member(
 
 
 INSERT INTO 
-tbl_read_book(isbn, title, author, publisher, price, discount, description, pubdate, link, image)
+tbl_read_book(isbn, title, author, publisher, price, discount, description, pubdate, link, image )
 VALUES
-('9791188850501' , '왕이된 남자 1' , '김선덕', '북라이프', '14,000' , '12,600', "" , "", "", "");
+('97911888505012' , '왕이된 남자 1' , '김선덕', '북라이프', '14,000' , '12,600', "" , "", "", "");
 
 INSERT INTO
 tbl_member(M_ID, M_PASSWORD, M_EMAIL, M_NAME)
--- nodejs900_Read_books/public/js/input.js
+++ nodejs900_Read_books/public/js/input.js
@@ -18,11 +18,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const btn_submit = form.querySelector("button");
 
     const inputs = form.querySelectorAll("input");
-    const isbn = inputs[ST_INDEX.ISBN];
-    const title = inputs[ST_INDEX.TITLE];
-    const author = inputs[ST_INDEX.AUTHOR];
-    const publisher = inputs[ST_INDEX.PUBLISHER];
-    const price = inputs[ST_INDEX.PRICE];
+    const isbn = inputs[BOOK_INDEX.ISBN];
+    const title = inputs[BOOK_INDEX.TITLE];
+    const author = inputs[BOOK_INDEX.AUTHOR];
+    const publisher = inputs[BOOK_INDEX.PUBLISHER];
+    const price = inputs[BOOK_INDEX.PRICE];
 
     const error_divs = document.querySelectorAll("div.book.error");
 
@@ -34,17 +34,23 @@ document.addEventListener("DOMContentLoaded", () => {
         if (result === "ERROR") {
             message = " * DB 오류";
         } else if (result === "있다") {
-            message = " * 이미 등록된 ISBN번입니다";
+            message = " * 이미 등록된 ISBN입니다";
         } else if (result === "없다") {
             message = " * 사용가능한 ISBN입니다";
             color = "blue";
         }
-    }
+        error_divs[ST_INDEX.ISBN].innerHTML = message;
+        error_divs[ST_INDEX.ISBN].style.color = color;
+
+        return color === "red";
+
+    };
+
     btn_submit?.addEventListener("click", async () => {
         error_divs.forEach((item) => (item.innerHTML = ""));
         if (!isbn.value) {
-            error_divs[ST_INDEX.ISBN].innerHTML =
-                "* ISBN을 반드시 입력하세요";
+            error_divs[BOOK_INDEX.ISBN].innerHTML =
+                "* ISBN을 반드시 입력해야 합니다";
             isbn.select();
             return false;
         } else {
@@ -55,25 +61,25 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
         if (!title.value) {
-            error_divs[ST_INDEX.TITLE].innerHTML =
-                "* 제목은 반드시 입력해야 합니다";
+            error_divs[BOOK_INDEX.TITLE].innerHTML =
+                "* 도서명은 반드시 입력해야 합니다";
             title.select();
             return false;
         }
         if (!author.value) {
-            error_divs[ST_INDEX.AUTHOR].innerHTML =
+            error_divs[BOOK_INDEX.AUTHOR].innerHTML =
                 "* 저자는 반드시 입력하세요";
             author.select();
             return false;
         }
         if (!publisher.value) {
-            error_divs[ST_INDEX.PUBLISHER].innerHTML =
+            error_divs[BOOK_INDEX.PUBLISHER].innerHTML =
                 "* 출판사를 반드시 입력하세요";
             publisher.select();
             return false;
         }
         if (!price.value) {
-            error_divs[ST_INDEX.PRICE].innerHTML =
+            error_divs[BOOK_INDEX.PRICE].innerHTML =
                 "* 가격을 반드시 입력하세요";
             price.select();
             return false;
@@ -86,7 +92,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const target = event.target;
         const value = target.value;
         if (!value) {
-            error_divs[ST_INDEX.ISBN].innerText =
+            error_divs[BOOK_INDEX.ISBN].innerText =
                 "* ISBN을 입력해 주세요";
             target.select();
             return false;
@@ -100,15 +106,23 @@ document.addEventListener("DOMContentLoaded", () => {
         EVENT_ISBN = true;
     });
 
-    title.addEventListener("blur", (event) => {
+    title.addEventListener("blur", async (event) => {
         if (!EVENT_ISBN) return false;
         const target = event.target;
         const value = target.value;
         if (!value) {
-            error_divs[ST_INDEX.TITLE].innerText = " * 도서명은 반드시 입력해야 합니다 "
-            title.select();
+            error_divs[BOOK_INDEX.TITLE].innerText = 
+                " * 도서명은 반드시 입력해야 합니다 "
+            target.select();
             return false;
+        } else {
+            const bRedYes = await title_valid(target);
+            if (bRedYes) {
+                target.select();
+                return false;
+            }
         }
+        EVENT_TITLE = true;
     });
 
     isbn.focus();
