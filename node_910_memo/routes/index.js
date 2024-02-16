470a53d69febce0ae721682455740cb15c05bde2
sh1nheewon <tlsgmldnjs00@naver.com>
2024-02-16 15:50:09 +0900

    memo

--- node_910_memo/routes/index.js
+++ node_910_memo/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import moment from "moment";
 import DB from "../models/index.js";
+import { upLoad } from '../modules/fileUpload.js';
 
 const MEMOS = DB.models.tbl_memos;
 const router = express.Router()
@@ -13,12 +14,22 @@ router.get('/', async (req, res, next) => {
   const toDate = moment().format("YYYY-MM-DD");
   const toTime = moment().format("HH:mm:ss");
 
+  try {
+    const rows = await MEMOS.findAll();
+    return res.render("index", { MEMOS: rows, toDate, toTime });
+  } catch (error) {
+    return res.json(error);
+  }
   // index.pug 를 rendering 할 때 사용하도록 보내주기
-  res.render('index', { toDate, toTime })
+  // res.render('index', { toDate, toTime })
 });
 
-router.post("/", async (req, res) => {
+router.post("/", upLoad.single("m_image"), async (req, res) => {
+  console.log(req.body);
+  const imageFile = req.file;
+  console.log(imageFile);
   try {
+    req.body.m_image = imageFile?.filename;
     req.body.m_author = "tlsgmldnjs00@naver.com";
     await MEMOS.create(req.body);
     return res.redirect("/");
@@ -35,6 +46,5 @@ router.get("/get_new_date", async (req, res) => {
   // return res.json({ toDate: toDate, toTime: toTime });
   return res.json({ toDate, toTime });
 });
-
 
 export default router
