fa3f8906aec0a19580fc1f4052a034fa566b0b27
sh1nheewon <tlsgmldnjs00@naver.com>
2024-02-19 11:00:14 +0900

    memo

--- node_910_memo/public/js/memo.js
+++ node_910_memo/public/js/memo.js
@@ -1,40 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const date_form = document.querySelector("form.date");
-    const input_form = document.querySelector("form.input");
-    const btn_save = document.querySelector("input.btn_save");
-    const btn_new = document.querySelector("input.btn_new");
+  const date_form = document.querySelector("form.date");
+  const input_form = document.querySelector("form.input");
+  const btn_save = document.querySelector("input.btn_save");
+  const btn_new = document.querySelector("input.btn_new");
 
-    btn_new.addEventListener("click", async () => {
-        try {
-            const response = await fetch("/get_new_date");
-            const json = await response.json();
-            date_form.querySelector("input.todate").value = json.toDate;
-            date_form.querySelector("input.totime").value = json.toTime;
-        } catch (error) {
-            alert("서버 통신오류");
-        }
-    });
+  btn_new.addEventListener("click", async () => {
+    try {
+      const response = await fetch("/get_new_date");
+      const json = await response.json();
+      date_form.querySelector("input.todate").value = json.toDate;
+      date_form.querySelector("input.totime").value = json.toTime;
+    } catch (error) {
+      alert("서버 통신오류");
+    }
+  });
 
-    btn_save.addEventListener("click", () => {
-        const toDate = date_form.querySelector("input.todate");//.value;
-        const toTime = date_form.querySelector("input.totime");//.value;
-        // const toSubject = input_form.querySelector("input.tosubject").value;
-        // const toMemo = input_form.querySelector("input.tomemo").value;
+  btn_save.addEventListener("click", () => {
+    const toDate = date_form.querySelector("input.todate"); // .value;
+    const toTime = date_form.querySelector("input.totime"); // .value;
+    // const toSubject =
+    //   input_form.querySelector("input.tosubject").value;
+    // const toMemo = input_form.querySelector("input.tomemo").value;
 
-        /*
-        저장 버튼을 클릭헀을때
-        저장 버튼은 input_form 에 포함되어 있는 버튼
-        저장 버튼을 클릭하면 input_form 에 있는 값은 서버로 전송이 가능하지만
-        date_form 에 있는 값은 함께 서버로 보내지 못한다
-        그래서 date_form 에 있는 2 개의 입력 박스(toDate, toTime)를
-        input_form 에 추가하고 같이 보내주어야 한다
-        */
-        // toDate 와 toTime 의 값을 input_form 에 추가시키려면 appendChild 를 사용함
-        input_form.appendChild(toDate)
-        input_form.appendChild(toTime)
-        input_form.submit();
+    /**
+     * 저장 버튼을 클릭했을때
+     * 저정버튼은 input_form 에 포함되어 있는 버튼
+     * 저장버튼을 클릭하면 input_form 에 있는 값은 서버로 전송이 가능하지만
+     * date_form 에 있는 값은 함께 보내지 못한다
+     * 그래서 date_form 에 있는 2개의 입력박스(toDate, toTime)을
+     * input_form 에 추가하고 같이 보내줘야 한다
+     */
+    input_form.appendChild(toDate);
+    input_form.appendChild(toTime);
+    input_form.submit();
 
-        // console.log(toDate, toTime, toSubject, toMemo);
-    });
-
+    // console.log(toDate, toTime, toSubject, toMemo);
+  });
 });
--- node_910_memo/routes/index.js
+++ node_910_memo/routes/index.js
@@ -1,27 +1,25 @@
-import express from 'express'
+import express from "express";
 import moment from "moment";
 import DB from "../models/index.js";
-import { upLoad } from '../modules/fileUpload.js';
-
+import { upLoad } from "../modules/fileUpload.js";
 const MEMOS = DB.models.tbl_memos;
-const router = express.Router()
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   /**
    * moment 를 사용하여 현재 날짜와 시간을 문자열로 getter
    */
   const toDate = moment().format("YYYY-MM-DD");
   const toTime = moment().format("HH:mm:ss");
-
   try {
     const rows = await MEMOS.findAll();
     return res.render("index", { MEMOS: rows, toDate, toTime });
   } catch (error) {
     return res.json(error);
   }
-  // index.pug 를 rendering 할 때 사용하도록 보내주기
-  // res.render('index', { toDate, toTime })
+  // index.pug 를 rendering 할때 사용하도록 보내주기
+  // res.render("index", { toDate, toTime });
 });
 
 router.post("/", upLoad.single("m_image"), async (req, res) => {
@@ -30,7 +28,7 @@ router.post("/", upLoad.single("m_image"), async (req, res) => {
   console.log(imageFile);
   try {
     req.body.m_image = imageFile?.filename;
-    req.body.m_author = "tlsgmldnjs00@naver.com";
+    req.body.m_author = "callor@callor.com";
     await MEMOS.create(req.body);
     return res.redirect("/");
   } catch (error) {
@@ -41,10 +39,9 @@ router.post("/", upLoad.single("m_image"), async (req, res) => {
 router.get("/get_new_date", async (req, res) => {
   const toDate = moment().format("YYYY-MM-DD");
   const toTime = moment().format("HH:mm:ss");
-
-  // JSON 의 변수(key) 이름과 value 의 이름이 같을 때에는 한번 생략 가능
+  // JSON 의 변수(key)이름과 value의 이름이 같을때는 한번 생략가능
   // return res.json({ toDate: toDate, toTime: toTime });
   return res.json({ toDate, toTime });
 });
 
-export default router
+export default router;
